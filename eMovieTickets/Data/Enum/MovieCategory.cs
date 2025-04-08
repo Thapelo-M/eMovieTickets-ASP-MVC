103bf233a400535444863cd47e9620265f19537b
Thapelo-M <keomothokoa@gmail.com>
2025-04-08 09:16:20 +0200

    Latest commit, with controller configured

---
+++ eMovieTickets/Data/Enum/MovieCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMovieTickets.Data.Enum
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
